b57baf8c99842eaf1bbbdcbe06b63035f8aaf8b7
Dhananjaydk <Dhananjaykhairnar15@gmail.com>
2022-04-12 09:30:20 +0530

    Program to check given no is palindrome or not

---
+++ Palindrome.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        System.out.print("\t* Enter The No. : ");
+        Scanner obj = new Scanner(System.in);
+        int no = obj.nextInt();
+        if(no == Palindrome(no))
+        System.out.println("\t* "+no+" is Palindrome.");
+    else
+    System.out.println("\t* "+no+" is Not Palindrome");
+    obj.close();
+    }
+    public static int Palindrome(int no){
+        int sum = 0 , rem ;
+        while (no > 0)
+        {
+            rem = no % 10;
+            sum = (sum * 10) + rem;
+            no = no / 10;
+        }
+        return sum;
+    }
+}
